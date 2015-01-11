d8210d4c3596db5db0d1d9345edaf4c0403d89ce
Yusuf Abdulla Shunan <shunan@maldicore.com>
2015-01-11 22:45:53 +0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Silverstripe Client Export
+[![Latest Stable Version](https://packagist.org/packages/maldicore/silverstripe-clientexport)]
+[![License](https://packagist.org/packages/maldicore/silverstripe-clientexport)]
+
+1. Exports all files and folders in the assets directory to a ZIP file in the CMS.
+2. Exports the database to a ZIP file in the CMS
+
+![Asset Export](http://cl.ly/image/1538202E0k3B/Image%202014-10-15%20at%2012.28.43%20pm.png)
+![Asset Export](http://cl.ly/image/1538202E0k3B/Image%202014-10-15%20at%2012.28.43%20pm.png)
+
+By Stan Hutcheon - [Bigfork Ltd](http://bigfork.co.uk) - Silverstripe AssetExport (https://github.com/stnvh/silverstripe-assetexport)
+By Maldicore Group Pvt Ltd - Database Export ()
+
+## Installation:
+
+### Composer:
+
+```
+composer require "maldicore/silverstripe-clientexport" "~1"
+```
+
+### Download:
+
+Clone this repo into a folder called ```clientexport``` in your silverstripe installation folder.
+
+### Usage:
+
+It adds 2 buttons in the 'Files' section of the CMS. Just click respective buttons to backup and download either the database or asset folder.
+
+After installing via composer, you must */dev/build*
---
+++ code/ClientAdminExport.php
@@ -0,0 +1,153 @@
+<?php
+
+class ClientAdminExport extends LeftAndMainExtension {
+
+	private static $allowed_actions = array(
+		'assetbackup','dbbackup'
+	);
+
+	/**
+	* @param Form $form
+	* @return Form $form
+	*/
+	public function updateEditForm(Form $form) {
+		$assetbackupButton = new LiteralField(
+			'AssetBackupButton',
+			sprintf(
+			'<a class="ss-ui-button ss-ui-action ui-button-text-icon-primary" data-icon="arrow-circle-135-left" title="%s" href="%s">%s</a>',
+			'Performs an asset backup in ZIP format. Useful if you want all assets and have no FTP access',
+			$this->owner->Link('assetbackup'),
+			'Backup Asset files'
+			)
+		);
+
+		$dbbackupButton = new LiteralField(
+			'DBBackupButton',
+			sprintf(
+			'<a class="ss-ui-button ss-ui-action ui-button-text-icon-primary" data-icon="arrow-circle-135-left" title="%s" href="%s">%s</a>',
+			'Performs a database backup in ZIP format. Useful if you want a dump of the latest version of the database',
+			$this->owner->Link('dbbackup'),
+			'Backup Database'
+			)
+		);
+
+		if($field = $this->fieldByExtraClass($form->Fields(), 'cms-actions-row')) {
+			$field->push($assetbackupButton);
+			$field->push($dbbackupButton);
+		}
+
+		return $form;
+	}
+
+
+	/**
+	* Recursively search & return a field by 'extra class' from FieldList.
+	*
+	* @todo Could be added as a FieldList extension but it's a bit overkill for the sake of a button
+	*
+	* @param FieldList $fields
+	* @param $class The extra class name to search for
+	* @return FormField|null
+	*/
+	public function fieldByExtraClass(FieldList $fields, $class) {
+		foreach($fields as $field)  {
+			if($field->extraClasses){
+				if(in_array($class, $field->extraClasses)) {
+					return $field;
+				}
+			}
+			if(method_exists($field, 'FieldList')) {
+				return $this->fieldByExtraClass($field->FieldList(), $class);
+			}
+		}
+	}
+
+	/**
+	* @return SS_HTTPRequest
+	*/
+	public function dbbackup() {
+		$mysqldump = 'mysqldump';
+		// TODO: check if mysqldump binaries exists
+		global $databaseConfig;
+		$siteConfig = SiteConfig::current_site_config();
+
+		// Backup database
+		$name = $databaseConfig['database'].'-'.date('Y-m-d-h-i-s').'.sql';
+		$tempName = ASSETS_PATH.DIRECTORY_SEPARATOR.$name;
+		$command = sprintf(
+		'%s --user=%s --password=%s %s > %s',
+		$mysqldump,
+		escapeshellarg($databaseConfig['username']),
+		($databaseConfig['password'] ? escapeshellarg($databaseConfig['password']):''),
+		escapeshellarg($databaseConfig['database']),
+		escapeshellarg($tempName)
+		);
+		shell_exec($command);
+		$zipName = $databaseConfig['database'].'-'.date('Y-m-d-h-i-s').'.zip';
+		$zipTmpName = ASSETS_PATH . DIRECTORY_SEPARATOR . $zipName;
+		$zip = new ZipArchive();
+
+		if(!$zip->open($zipTmpName, ZipArchive::OVERWRITE)) {
+			user_error('Client Export Extension: Unable to read/write temporary zip archive', E_USER_ERROR);
+			return;
+		}
+
+		$zip->addFile($tempName, $zipName);
+
+		if(!$zip->status == ZipArchive::ER_OK) {
+			user_error('Client Export Extension: ZipArchive returned an error other than OK', E_USER_ERROR);
+			return;
+		}
+
+		$zip->close();
+
+		ob_flush(); // fix browser crash(?)
+
+		$content = file_get_contents($zipTmpName);
+		unlink($zipTmpName);
+		// Leave the database Backup in Folder for now TODO: set via config
+		// unlink($tempName);
+
+		return SS_HTTPRequest::send_file($content, $zipName);
+	}
+
+	/**
+	* @return SS_HTTPRequest
+	*/
+	public function assetbackup() {
+		$name = 'assets_' . SS_DateTime::now()->Format('Y-m-d') . '.zip';
+		$tmpName = TEMP_FOLDER . '/' . $name;
+		$zip = new ZipArchive();
+
+		if(!$zip->open($tmpName, ZipArchive::OVERWRITE)) {
+			user_error('Client Export Extension: Unable to read/write temporary zip archive', E_USER_ERROR);
+			return;
+		}
+
+		$files = new RecursiveIteratorIterator(
+		new RecursiveDirectoryIterator(
+		ASSETS_PATH,
+		RecursiveDirectoryIterator::SKIP_DOTS
+		)
+		);
+
+		foreach($files as $file) {
+			$local = str_replace(ASSETS_PATH . '/', '', $file);
+			$zip->addFile($file, $local);
+		}
+
+		if(!$zip->status == ZipArchive::ER_OK) {
+			user_error('Client Export Extension: ZipArchive returned an error other than OK', E_USER_ERROR);
+			return;
+		}
+
+		$zip->close();
+
+		ob_flush(); // fix browser crash(?)
+
+		$content = file_get_contents($tmpName);
+		unlink($tmpName);
+
+		return SS_HTTPRequest::send_file($content, $name);
+	}
+}
